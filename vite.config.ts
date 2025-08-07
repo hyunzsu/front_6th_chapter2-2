473ea60badaf1cd130f3506e8931692bbc939e36
현지수 <hyunzsu@naver.com>
2025-08-08 02:35:12 +0900

    feat: 404 페이지 생성

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,21 @@ import react from '@vitejs/plugin-react-swc';
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: '/front_6th_chapter2-1/', // 프로덕션용 고정값
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        input: {
+          main: 'index.html',
+        },
+      },
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
   })
-)
+);
